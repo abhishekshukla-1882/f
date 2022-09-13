7ab751cf8f21910bd756451ec85c22ba189e6c51
abhishekshukla <abhishekshukla2022jan@cedcoss.com>
2022-09-13 18:06:43 +0530

    d

---
+++ r/app/etc/aws.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'version' => 'latest',
+    'region' => 'ap-northeast-3', // for local (Singapor)
+    //    'region'      => 'us-east-2', // for Live Use (Ohio)
+    'credentials' => [
+        'key' => 'AKIAYQBKXVXDRS56U7TL', //pushpa
+        'secret' => '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', //pushpa
+    ],
+];
+// return [
+//     'version' => 'latest',
+//     'region' => 'ca-central-1', // for local (Singapor)
+//     'credentials' => [
+//                  'key'    => '', 
+//                 'secret' => ''
+//     ],
+// ];
