04f9dc276734d26f49c59cb1e12bc12bd8d31a44
abhishekshukla <abhishekshukla2022jan@cedcoss.com>
2022-09-14 12:08:57 +0530

    l

---
+++ untitled folder/r/app/etc/dynamo.php
@@ -0,0 +1,21 @@
+<?php
+
+// return [
+//     'version' => 'latest',
+//     'region' => 'enter_your_aws_region',
+//     'credentials' => [
+//       'key'    => 'enter_your_aws_access_key', 
+//       'secret' => 'enter_your_aws_secret_key'
+//     ],
+// ];
+
+
+return [
+  'version' => 'latest',
+  'region' => 'ap-northeast-3', // for local (Singapor)
+    //  'region'      => 'us-east-2', // for Live Use (Ohio)
+  'credentials' => [
+      'key' => 'AKIAYQBKXVXDRS56U7TL', //pushpa
+      'secret' => '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', //pushpa
+  ],
+];
---
+++ untitled folder/r/app/etc/modules.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'webapi' => 1,
+  'core' => 1,
+  'shopifywebapi' => 1,
+  'connector' => 1,
+  'apiconnect' => 1,
+  'amazonwebapi' => 1,
+  'cedcommercewebapi' => 1,
+);
